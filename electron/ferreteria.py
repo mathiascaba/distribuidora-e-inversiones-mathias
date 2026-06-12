b808c74267ec0c5dd4cb17fa0de902b8d4d99415
Mathias Cabanillas <mathiascabanillas23@gmail.com>
2026-06-11 20:33:21 -0500

    exe portable con pywebview (15MB standalone)

---
+++ electron/ferreteria.py
@@ -0,0 +1,11 @@
+import webview
+
+webview.create_window(
+    title="Ferreteria Mathias",
+    url="https://distribuidoraeinverisones.netlify.app",
+    width=1280,
+    height=900,
+    resizable=True,
+    fullscreen=False
+)
+webview.start()
